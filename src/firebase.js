86f7418c058e1d97febd6ee2c474a6e0d26e2215
myeongwon <lighting_one@hanmail.net>
2018-03-25 12:30:00 +0900

    [firebase test2] auth: google login, database: write, read

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase frome 'firebase';
+
+const config = {
+  apiKey: "AIzaSyBlxWEnoTm-oaC1zpLY0HBVSH5N5tfYAPA",
+  authDomain: "plan-5c103.firebaseapp.com",
+  databaseURL: "https://plan-5c103.firebaseio.com",
+  projectId: "plan-5c103",
+  storageBucket: "plan-5c103.appspot.com",
+  messagingSenderId: "715877448946"
+};
+firebase.initializeApp(config);
+
+export default config;
